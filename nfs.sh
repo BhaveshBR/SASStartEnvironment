267e6785a5285c7236a6ae2cc0c1be4ac6c785d9
Bhavesh Raval <bhavesh.raval@sas.com>
2022-05-26 10:52:13 +0530

    c

---
+++ nfs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+Namespace=$NS
+cat output.json | jq -r '@sh "export ipaddress=\(.jump_public_ip.value)"' > env.sh
+source env.sh
+SCRIPT="[[ -d /viya-share ]] && echo -e \"\nGood to go!\" || echo -e \"\nWhoa! Stop! Something is wrong with the NFS server.\""
+SCRIPTNS="mkdir /viya-share/NS && cd /viya-share/NS && mkdir bin data homes astores && ls -l /viya-share /viya-share/NS"
+HOSTS=$ipaddress
+SCR=${SCRIPTNS/NS/$Namespace}
+echo $SCR
+USERNAMES="jumpuser"
+sshpass ssh -l $USERNAMES $HOSTS "${SCRIPT}"
+sshpass ssh -l $USERNAMES $HOSTS "${SCR}"
+
